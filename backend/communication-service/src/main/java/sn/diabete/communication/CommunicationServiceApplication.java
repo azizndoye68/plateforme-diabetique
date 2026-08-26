af34a69b341ebb914836e6597fe4940d97b5457e
azizndoye68 <azizndoye68@gmail.com>
2026-08-26 16:00:26 +0000

    version 2.0 changement dépôt, reviens en local

--- backend/communication-service/src/main/java/sn/diabete/communication/CommunicationServiceApplication.java
+++ backend/communication-service/src/main/java/sn/diabete/communication/CommunicationServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class CommunicationServiceApplication {
+	public class CommunicationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunicationServiceApplication.class, args);
